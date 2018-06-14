1c202345dfecbb4cfeef9883e97a3dc39866bfc2
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2018-06-14 19:10:43 +0300

    LWDEV-7883 contrac 1.2.2 has been supported

---
+++ src/Lykke.Service.BlockchainApi.Client/TransactionAlreadyBroadcastedException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Lykke.Service.BlockchainApi.Client
+{
+    /// <summary>
+    /// This exception could be thrown on transaction building, if transaction has been alread
+    /// broadcasted or even removed
+    /// </summary>
+    [PublicAPI]
+    [Serializable]
+    public class TransactionAlreadyBroadcastedException : Exception
+    {
+        /// <summary>
+        /// This exception could be thrown on transaction building, if transaction has been alread
+        /// broadcasted or even removed
+        /// </summary>
+        public TransactionAlreadyBroadcastedException(ErrorResponseException ex) :
+            base("Transaction has been already broadcasted or removed", ex)
+        {
+        }
+    }
+}
